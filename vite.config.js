4767bc9e4dcaea81f62d367cc7e86d9d95172ee5
Hammad Mustafa <hammadmustafa8401@gmail.com>
2025-01-16 18:29:23 +0500

    vite.config.js modified

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hammad-portfolio/', // Update this to your Vercel project name
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default output folder
-    sourcemap: false, // Disable source maps for a smaller build
-    assetsInlineLimit: 4096, // Inline assets smaller than 4kb
-  },
 });
